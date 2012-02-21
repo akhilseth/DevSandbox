d795f2fae5a913788d29fae12d72f457560b095b
Akhil Seth <akhil@tacc.utexas.edu>
2012-02-21 10:43:49 -0600

    Implemented Greeter class

---
+++ HelloWorld/Greeter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class Greeter
+    {
+        protected string _greeting;
+
+        public Greeter(string greeting)
+        {
+            _greeting = greeting;
+        }
+
+        public string Greet(string name)
+        {
+            return _greeting + ", " + name;
+        }
+    }
+}
